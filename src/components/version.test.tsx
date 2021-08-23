0b38daec0526a137d7fc71b43ce9385669184b5c
Yukio Nozawa <personal@nyanchangames.com>
2021-08-23 23:23:20 +0900

    Add version component

---
+++ src/components/version.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Version from "./version";
+import { version } from "../version";
+
+test('renders version', () => {
+  render(<Version />);
+  const e = screen.getByText("Version: " + version);
+  expect(e).toBeInTheDocument();
+});
---
+++ src/components/version.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { version } from "../version";
+
+export default function Version() {
+  return <div className="Version">Version: {version}</div>;
+}
