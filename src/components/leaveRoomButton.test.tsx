55892405bbb97421b96a20b9528ead04f7ed23b6
Yukio Nozawa <personal@nyanchangames.com>
2022-06-01 00:09:50 +0900

    Fix lint warning

--- src/components/leaveRoomButton.test.tsx
+++ src/components/leaveRoomButton.test.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import LeaveRoomButton from "./leaveRoomButton";
 import { createGlobalLogicForTest } from "../testHelper";
-import { act } from "react-dom/test-utils";
 
 test("show confirmation when expanded", () => {
   const gl = createGlobalLogicForTest();
   render(<LeaveRoomButton globalLogic={gl} onLeave={() => {}} />);
   const e = screen.getByText("ルームを退室");
   expect(e).toBeInTheDocument();
-  act(() => {
-    fireEvent(e, new MouseEvent("click", { bubbles: true, cancelable: true }));
-  });
+  fireEvent(e, new MouseEvent("click", { bubbles: true, cancelable: true }));
   const c = screen.getByText(/本当に/);
   expect(c).toBeInTheDocument();
 });
